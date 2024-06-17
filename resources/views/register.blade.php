7b10805c87c19cc48b9afe7c2979abbc605ac637
jozzamars <jossaadvincula7@mail.com>
2024-06-17 09:37:25 +0800

    started building the UI

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,38 +1,38 @@
 @extends('layout')
-@section('title', 'register')
-@section('content')
-<!-- because in the login the yield is set to content -->
-    <div class="container">
-    <form style="width:500px" > 
-    <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">FirstName</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">Enter your FirstName</div>
-  </div>
-    <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-    </div>
 
+@section('title', 'Registration')
+
+@section('content')
+    <div class="container">
+        <form style="width: 500px;">
+            <div class="mb-3">
+                <label for="firstName" class="form-label">First Name</label>
+                <input type="text" class="form-control" id="firstName" aria-describedby="firstNameHelp">
+                <div id="firstNameHelp" class="form-text">Enter your First Name</div>
+            </div>
+            <div class="mb-3">
+                <label for="lastName" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="lastName" aria-describedby="lastNameHelp">
+                <div id="lastNameHelp" class="form-text">Enter your Last Name</div>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">Enter your email</div>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <div class="mb-3">
+                <label for="passwordConfirmation" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="passwordConfirmation">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\UserController;
+// use App\Http\Controllers\SubjectsController;
+// use App\Http\Controllers\UserSubjectController;
+// use App\Http\Controllers\MaterialsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +26,32 @@ Route::get('/', function () {
 Route::get('/login', function() {
     return view('login');
 });
+
+Route::get('/register', function(){
+    return view('register');
+});
+
+
+// Route::post("/register", [AuthController::class, "register"]);
+// Route::post("/login", [AuthController::class, "login"]);
+// Route::post("/logout", [AuthController::class, "logout"])->middleware('auth:sanctum');
+
+// //user Routes
+// Route::prefix('users')->group(function () {
+//     Route::get('/', [UserController::class, 'index']);
+//     Route::get('/{id}', [UserController::class, 'show']);
+//     Route::put('/{id}', [UserController::class, 'update']);
+//     Route::delete('/{id}', [UserController::class, 'destroy']);
+//     Route::get('/search/{name}', [UserController::class, 'search']);
+// });
+
+// Route::get('/user/{id}/subjects-materials', [UserSubjectController::class, 'show']);
+
+// Route::put('/material/{id}', [MaterialsController::class, 'update']);
+// Route::get('/materials', [MaterialsController::class, 'index']);
+
+// Route::get('/subjects', [SubjectsController::class, 'index']);
+// Route::put('/subjects/{id}', [SubjectsController::class, 'update']);
+
+
+
